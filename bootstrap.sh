3679db38f2404564c1aae9956217d7a8a6a336c5
Jonathan Lozinski <jonathan.lozinski@gmail.com>
2014-07-11 17:09:26 -0400

    Added a convenience command - dex

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,11 @@
 sudo apt-get update
 sudo apt-get install vim tmux
 
+echo "creating Development setup"
+mkdir ~/Development
+mkdir ~/bin
+cp scripts/* ~/bin/
+
 echo "Setting up profile"
 echo "" >> ~/.bashrc
 cat dotfiles/bashrc >> ~/.bashrc
